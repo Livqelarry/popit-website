a375cffc58f6077da0f68465751f7064fd315c21
Praditya <pradityaaldi.s@gmail.com>
2022-11-24 09:11:29 +0700

    last update
    update comment, and last review before submit

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = () => {
-  document.getElementById("navTriggerMob").onclick = () => {
-    document.getElementById("nav").classList.toggle("header__nav--show");
-  };
-};
-
-toggleMenu();
